3d27124b694924b74cd657a650fb5b6404b23bda
rhf <2427219623@qq.com>
2022-07-02 21:51:14 +0800

    add

--- jianzhioffer/letterCombinations.py
+++ jianzhioffer/letterCombinations.py
@@ -7,8 +7,6 @@ class Solution:
         if not digits:
             return []
 
-        digits = list(digits)
-
         digits_dict = {}
         for num in digits:
             if num in digits_dict.keys():
@@ -53,6 +51,36 @@ class Solution:
         get_str(0, "")
 
         return result
+
+    def letterCombinations1(self, digits: str) -> List[str]:
+        if not digits:
+            return list()
+
+        phoneMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(index: int):
+            if index == len(digits):
+                combinations.append("".join(combination))
+            else:
+                digit = digits[index]
+                for letter in phoneMap[digit]:
+                    combination.append(letter)
+                    backtrack(index + 1)
+                    combination.pop()
+
+        combination = list()
+        combinations = list()
+        backtrack(0)
+        return combinations
 
 
 if __name__ == "__main__":
